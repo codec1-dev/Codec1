27e84879913f394ecd407a275f239004ee5e1258
Codec1 <info.codecone1@gmail.com>
2025-03-10 11:21:50 +0530

    some bug fixes

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,18 @@
+// Function to load header and footer
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("header.html")
+      .then(response => response.text())
+      .then(data => {
+          document.getElementById("header").innerHTML = data;
+      });
+
+  fetch("footer.html")
+      .then(response => response.text())
+      .then(data => {
+          document.getElementById("footer").innerHTML = data;
+      });
+});
+
 // Dark Mode Toggle
 document.addEventListener("DOMContentLoaded", () => {
   const toggleButton = document.createElement("button");
